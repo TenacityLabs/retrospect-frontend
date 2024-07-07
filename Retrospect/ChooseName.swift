0523b8bb7a8cec3e80d499d8b3902742cb928a5a
Andrew Durnford <andrewhdurnford@gmail.com>
2024-07-07 17:05:54 -0400

    styled login page

--- Retrospect/ChooseName.swift
+++ Retrospect/ChooseName.swift
@@ -46,7 +46,6 @@ struct ChooseName: View {
             }
             Spacer()
         }
-        .padding(.horizontal, 20)
     }
 }
 
--- Retrospect/Login.swift
+++ Retrospect/Login.swift
@@ -12,33 +12,106 @@ struct Login: View {
     @State private var password: String = ""
     @State private var loginResult: String = ""
     @State private var jwt: String = ""
+    @State private var isPasswordVisible: Bool = false
     
     var body: some View {
         VStack {
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
             
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
+            Image("Logo White")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+            
+            Text("Login")
+                .font(.custom("IvyOraDisplay-Regular", size: 48))
+                .foregroundColor(.white)
+            
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                
+                TextField("Email", text: $email)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email").foregroundColor(.gray)
+                    }
+                    .accentColor(.white)
+            }
+            .padding()
+            .frame(maxHeight: 60)
+            .background(Color.black.opacity(0.5))
+            .cornerRadius(30)
+            .padding(.horizontal, 40)
+            .padding(.bottom, 10)
+            
+            HStack {
+                Image(systemName: "lock")
+                    .foregroundColor(.gray)
+                
+                if isPasswordVisible {
+                    TextField("", text: $password)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                        .placeholder(when: password.isEmpty) {
+                            Text("Password").foregroundColor(.gray)
+                        }
+                        .accentColor(.white)
+                } else {
+                    SecureField("", text: $password)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                        .placeholder(when: password.isEmpty) {
+                            Text("Password").foregroundColor(.gray)
+                        }
+                        .accentColor(.white)
+                }
+                
+                Button(action: {
+                    isPasswordVisible.toggle()
+                }) {
+                    Image(systemName: isPasswordVisible ? "eye.slash" : "eye")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.gray)
+                        .frame(width: 20, height: 20)
+                }
+                .padding(.trailing, 10)
+            }
+            .padding()
+            .frame(maxHeight: 60)
+            .background(Color.black.opacity(0.5))
+            .cornerRadius(30)
+            .padding(.horizontal, 40)
+            .padding(.bottom, 80)
             
             Button(action: {
                 login()
             }) {
                 Text("Login")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                    .font(.custom("Syne-Regular", size: 18))
+                    .foregroundColor(Color.black)
             }
             .padding()
+            .frame(maxWidth: .infinity, maxHeight: 60)
+            .background(Color.white)
+            .cornerRadius(30)
+            .padding(.horizontal, 30)
+            .padding(.bottom, 5)
             
-            Text(loginResult)
-                .padding()
-                .foregroundColor(.red)
+            HStack {
+                Text("Don't have an account?")
+                    .foregroundColor(.white)
+                Button(action: {
+//                    state = "SignUp"
+                }) {
+                    Text("Sign Up")
+                        .foregroundColor(Color(red: 128/255, green: 128/255, blue: 128/255))
+                        .fontWeight(.bold)
+                        .underline()
+                }
+            }
         }
-        .padding()
     }
     
     private func login() {
@@ -46,10 +119,10 @@ struct Login: View {
             DispatchQueue.main.async {
                 switch result {
                 case .success(let token):
-                    loginResult = "Login successful! Token: \(token)"
+                    loginResult = "Login successful!"
                     jwt = token.token
-                case .failure(let error):
-                    loginResult = "Login failed: \(error)"
+                case .failure(_):
+                    loginResult = "Login failed, please try again!"
                 }
             }
         }
@@ -58,5 +131,8 @@ struct Login: View {
 
 
 #Preview {
-    Login()
+    ZStack {
+        BackgroundImageView()
+        Login()
+    }
 }
